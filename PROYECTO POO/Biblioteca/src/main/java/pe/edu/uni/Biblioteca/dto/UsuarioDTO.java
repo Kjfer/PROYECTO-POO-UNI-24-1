cf2e33867b435a01d9978890b30d67a9684e5097
Jose <josejcornejo.03@gmail.com>
2024-06-02 20:54:04 -0500

    Login controller, service y DTO de usuario

---
+++ PROYECTO POO/Biblioteca/src/main/java/pe/edu/uni/Biblioteca/dto/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package pe.edu.uni.BIBLIOTECA.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class UsuarioDTO {
+
+	private String codigo;
+	private String password;
+	private String tipo;
+	private boolean activa;
+	
+}
